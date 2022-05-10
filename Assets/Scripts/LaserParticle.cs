b037b73eca4ca8aec9d232e1a57d7f735f27611d
Sir Flynn <flynnvanstryp@gmai.com>
2022-05-10 18:39:42 +1000

    Laser line in progress

--- Assets/Scripts/LaserParticle.cs
+++ Assets/Scripts/LaserParticle.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class LaserParticle : MonoBehaviour
 {
-    
+    [SerializeField] private float defDistanceRay = 100;
+    public Transform laserFirePoint;
+    public LineRenderer m_lineRenderer;
+    Transform m_transform;
+
+    private void Awake()
+    {
+        m_transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        if (Physics2D.Raycast(m_transform.position, -transform.up))
+        {
+            RaycastHit2D _hit = Physics2D.Raycast(m_transform.position, -transform.up);
+            Draw2DRay(laserFirePoint.position, _hit.point);
+        }
+        else
+        {
+            Draw2DRay(laserFirePoint.position, laserFirePoint.transform.up * defDistanceRay);
+        }
+    }
+
+    void Draw2DRay(Vector2 startpos, Vector2 endpos)
+    {
+        m_lineRenderer.SetPosition(0, startpos);
+        m_lineRenderer.SetPosition(1, endpos);
+    }
 }
