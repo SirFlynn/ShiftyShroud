95cee1b1e7c24df8a9a9f5f9cfe8e7f83900888a
Sir Flynn <flynnvanstryp@gmai.com>
2022-05-23 12:47:59 +1000

    main menu UI functional
    All buttons work, sliders still need functionality

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,59 +7,80 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject mainMenuUI;
-    public GameObject levelMenuUI;
-    public GameObject creditsMenuUI;
-    public GameObject settingsMenuUI;
+    // public GameObject mainMenuUI;
+    // public GameObject levelMenuUI;
+    // public GameObject creditsMenuUI;
+    // public GameObject settingsMenuUI;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        mainMenuUI = GameObject.Find("MainMenuUI");
-        levelMenuUI = GameObject.Find("LevelMenuUI");
-        settingsMenuUI = GameObject.Find("SettingsMenuUI");
-        creditsMenuUI = GameObject.Find("CreditsMenuUI");
+    //void Start()
+    //{
+    //    mainMenuUI = GameObject.Find("MainMenuUI");
+    //    levelMenuUI = GameObject.Find("LevelMenuUI");
+    //    settingsMenuUI = GameObject.Find("SettingsMenuUI");
+    //    creditsMenuUI = GameObject.Find("CreditsMenuUI");
+    //
+    //    mainMenuUI.SetActive(true);
+    //    levelMenuUI.SetActive(false);
+    //    settingsMenuUI.SetActive(false);
+    //    creditsMenuUI.SetActive(false);
 
-        mainMenuUI.SetActive(true);
-        levelMenuUI.SetActive(false);
-        settingsMenuUI.SetActive(false);
-        creditsMenuUI.SetActive(false);
-    }
 
-    public void PlayGame()
-    {
-        mainMenuUI.SetActive(false);
-        levelMenuUI.SetActive(true);
-    }
+    //public void PlayGame()
+    //{
+    //    mainMenuUI.SetActive(false);
+    //    levelMenuUI.SetActive(true);
+    //}
 
-    public void Return()
-    {
-        mainMenuUI.SetActive(true);
-        levelMenuUI.SetActive(false);
-        creditsMenuUI.SetActive(false);
-        settingsMenuUI.SetActive(false);
-    }
+    //public void Return()
+    //{
+    //    mainMenuUI.SetActive(true);
+    //    levelMenuUI.SetActive(false);
+    //    creditsMenuUI.SetActive(false);
+    //    settingsMenuUI.SetActive(false);
+    //}
 
-    public void Settings()
-    {
-        mainMenuUI.SetActive(false);
-        settingsMenuUI.SetActive(true);
-    }
+    //public void Settings()
+    //{
+    //    mainMenuUI.SetActive(false);
+    //   settingsMenuUI.SetActive(true);
+    //}
 
-    public void Credits()
-    {
-        mainMenuUI.SetActive(false);
-        creditsMenuUI.SetActive(true);
-    }
+    //public void Credits()
+    //{
+    //    mainMenuUI.SetActive(false);
+    //    creditsMenuUI.SetActive(true);
+    //}
 
     public void Quit()
     {
+        Debug.Log("Quit Game");
         Application.Quit();
     }
 
     public void Level1()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("Level_01");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level_02");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level_03");
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level_04");
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene("Level_05");
     }
 
     public void MainMenu()
