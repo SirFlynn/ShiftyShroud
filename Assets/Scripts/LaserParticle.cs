32d4a5762868b58944de0e711d0bce16b96ff35d
Sir Flynn <flynnvanstryp@gmai.com>
2022-05-23 15:23:13 +1000

    Changes to levels 1 2 and 3
    Ordered Hierarchy for level 1 2 and 3  
    Added UI functionality for 1 and 2
    Fixed Laser being angled in levels by creating an end point

--- Assets/Scripts/LaserParticle.cs
+++ Assets/Scripts/LaserParticle.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class LaserParticle : MonoBehaviour
 {
     [SerializeField] private float defDistanceRay = 100;
-    public Transform laserFirePoint;
+    public Transform laserFireStart;
+    public Transform laserFireEnd;
     public LineRenderer m_lineRenderer;
     Transform m_transform;
 
@@ -33,12 +34,12 @@ public class LaserParticle : MonoBehaviour
         if (Physics2D.Raycast(m_transform.position, -transform.up, 10, layerMask))
         {
             RaycastHit2D hit = Physics2D.Raycast(m_transform.position, -transform.up);
-            Draw2DRay(laserFirePoint.position, hit.point);
+            Draw2DRay(laserFireStart.position, hit.point);
 
             //checks if the object the raycast hits is the player and if it is triggers level fail.
             if (hit.transform.CompareTag("Player"))
             {
-                Debug.Log("you hit me");
+                //Debug.Log("you hit me");
                 gameManager.LevelFailed();
                 //change sprite to deathsprite
             }
@@ -46,7 +47,7 @@ public class LaserParticle : MonoBehaviour
 
         else
         {
-            Draw2DRay(laserFirePoint.position, laserFirePoint.transform.up * defDistanceRay);
+            Draw2DRay(laserFireStart.position, laserFireEnd.position * defDistanceRay);
         }
     }
 
