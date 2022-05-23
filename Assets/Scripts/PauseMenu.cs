f2af3e4ef946f8cbf6f9b858f92dcdfbfd1ad93f
Sir Flynn <flynnvanstryp@gmai.com>
2022-05-23 16:13:31 +1000

    Fixed Pause menu scripts and UI functionality in level 1 and 2

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -23,7 +23,7 @@ public class PauseMenu : MonoBehaviour
             }
         }
     }
-    void Resume ()
+    public void Resume ()
     {
         Time.timeScale = 1f;
         GameIsPaused = false;
