3026f6baae3e6f2f8d2f0690858e430e8c287c43
Sir Flynn <flynnvanstryp@gmai.com>
2022-06-06 15:26:47 +1000

    Mouse can now faces the direction it is moving

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -7,78 +7,76 @@ public class MouseController : MonoBehaviour
     public float moveSpeed = 5f;
     public Vector3 movePoint;
 
-    //public GameObject endWaypoint;
-    //public GameObject player;
-
-    public FlipMovementSprite flipSprite;
-    public float horizontal;
-    public float vertical;
-
     public float duration = 4.0f;
 
     public LayerMask whatStopsMovement;
 
     public GameManager gameManager;
 
+    float inputHorizontal;
+    float inputVertical;
+
+    public Transform mouseSprite;
 
     void Start()
     {
         movePoint = transform.position;
     }
 
-    /*Lerps the player to the cheese 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Cheese")
-        {
-            if (duration == 0)
-                duration = 1;
-
-            //goes from 0-durations value then repeats
-            float timer = Mathf.PingPong(Time.time, duration) / duration;
-
-            //change timer to ease the acceleration
-            timer = easeInOutSine(timer);
-
-            //Lerp position from start waypoint to end waypoint over time
-            Vector3 position = Vector3.Lerp(player.transform.position, endWaypoint.transform.position, timer);
-
-            transform.position = position;
-
-            //used to slow down the object and make it gain speed when moving
-            float easeInOutSine(float x)
-            {
-                return -(Mathf.Cos(Mathf.PI * x) - 1) / 2;
-            }
-        }
-    }
-    */
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint, moveSpeed * Time.deltaTime);
+        
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
 
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
 
         if (Vector3.Distance(transform.position, movePoint) <= .05f)
         {
-
+            //controls horizontal movement
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
+                //stops movement if collides with whatStopsMovement
                 if (!Physics2D.OverlapCircle(movePoint + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
                 {
+                    //moves the movepoint to the space specified with WASD then moves everything to that movepoint
                     movePoint += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
 
-                    transform.eulerAngles = new Vector3(0, 0, -90);
+                    //Changes the rotation transform of the mouse sprite to face to the right 
+                    if(inputHorizontal >0)
+                    {
+                        mouseSprite.eulerAngles = new Vector3(0, 0, -90);
+                    }
+
+                    //Changes the rotation transform of the mouse sprite to face to the left 
+
+                    if (inputHorizontal < 0)
+                    {
+                        mouseSprite.eulerAngles = new Vector3(0, 0, 90);
+                    }
                 }
             }
+
+            //controls vertical movement
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
                 if (!Physics2D.OverlapCircle(movePoint + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
                 {
                     movePoint += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
 
-                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    //Changes the rotation transform of the mouse sprite to face up
+                    
+                    if (inputVertical > 0)
+                    {
+                        mouseSprite.eulerAngles = new Vector3(0, 0, 0);
+                    }
+
+                    //Changes the rotation transform of the mouse sprite to face down
+
+                    if (inputVertical < 0)
+                    {
+                        mouseSprite.eulerAngles = new Vector3(0, 0, -180);
+                    }
                 }
             }
         }
