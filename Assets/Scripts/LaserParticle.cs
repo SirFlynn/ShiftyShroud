c0fec84bb995ca83d3c1146fc801086dc70dbdf2
Sir Flynn <flynnvanstryp@gmai.com>
2022-06-07 13:49:53 +1000

    Laser Sparks now destroy upon finishing play and walls have collision

--- Assets/Scripts/LaserParticle.cs
+++ Assets/Scripts/LaserParticle.cs
@@ -35,11 +35,13 @@ public class LaserParticle : MonoBehaviour
         {
             RaycastHit2D hit = Physics2D.Raycast(m_transform.position, -transform.up, defDistanceRay, layerMask);
             Draw2DRay(laserFireStart.position, hit.point);
+            
 
-            Instantiate(laserSparks, hit.transform);
+            //makes sparks appear at the end of the Raycast
+            Instantiate(laserSparks, hit.point, Quaternion.identity);
 
             //checks if the object the raycast hits is the player and if it is triggers level fail.
-            if (hit.transform.CompareTag("Player"))
+                if (hit.transform.CompareTag("Player"))
             {
                 //Debug.Log("you hit me");
                 gameManager.LevelFailed();
